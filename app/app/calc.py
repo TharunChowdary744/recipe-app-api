97af544e05b7348d5da7c84548237b350b491ad5
TharunAnne <tharun.anne@dizitiveit.com>
2024-05-10 15:45:04 +0530

    Completed today office class

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator
+"""
+
+def add(x,y):
+    """Add x and y and return result."""
+    return x + y
+
+def substract(x,y):
+    """Subtract x and y and return result."""
+    return x - y
