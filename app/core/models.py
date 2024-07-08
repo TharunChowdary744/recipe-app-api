cf20ab162c1215fa08ae6252acf1801b13f3d7b6
TharunAnne <tharun.anne@dizitiveit.com>
2024-07-08 18:36:40 +0530

    working on filtering based on tags and Ingredients

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,6 @@
-"""Database modals."""
+"""
+Database models.
+"""
 import uuid
 import os
 
@@ -10,15 +12,17 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
-def recipe_image_file_path(instance,filename):
+
+def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image."""
     ext = os.path.splitext(filename)[1]
     filename = f'{uuid.uuid4()}{ext}'
 
     return os.path.join('uploads', 'recipe', filename)
 
+
 class UserManager(BaseUserManager):
-    """Manager for Users"""
+    """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
@@ -31,7 +35,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """create and return a new superuser"""
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -51,6 +55,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
+
 class Recipe(models.Model):
     """Recipe object."""
     user = models.ForeignKey(
@@ -58,16 +63,17 @@ class Recipe(models.Model):
         on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=255)
-    description = models.TextField(blank = True)
+    description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
-    price = models.DecimalField(max_digits=5,decimal_places=2)
-    link = models.CharField(max_length=225, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
     image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
+
 
 class Tag(models.Model):
     """Tag for filtering recipes."""
@@ -80,8 +86,9 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
-    """Ingredient for recipes"""
+    """Ingredient for recipes."""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
